287a83002f384db826cfa729eae7ca20d1d9e114
root <root@DESKTOP-5EBSNVF.localdomain>
2021-08-21 06:40:03 +0700

    add validation form to post without attachment and hastag

--- user_spec.rb
+++ user_spec.rb
@@ -1,5 +1,7 @@
+require 'simplecov'
+SimpleCov.start
 require './models/user'
-require 'rspec'
+require './models/post'
 
 describe User do
   describe '#valid?' do
@@ -11,6 +13,50 @@ describe User do
                           bio: "thomas adalah teman arif"
                         })
         expect(user.valid?).to eq(true)
+      end
+    end
+
+    context 'when initializerd with invalid username' do
+      it 'should return false' do
+        user = User.new({
+                          email: "thomas12@gmail.com",
+                          bio: "thomas adalah teman arif"
+                        })
+        expect(user.valid?).to eq(false)
+      end
+    end
+
+    context 'when initializerd with invalid email' do
+      it 'should return false' do
+        user = User.new({
+                          username: "thomas12",
+                          bio: "thomas adalah teman arif"
+                        })
+        expect(user.valid?).to eq(false)
+      end
+    end
+
+    context 'when initializerd with invalid bio' do
+      it 'should return false' do
+        user = User.new({
+                          username: "thomas12",
+                          bio: "thomas adalah teman arif"
+                        })
+        expect(user.valid?).to eq(false)
+      end
+    end
+  end
+end
+
+describe Post do
+  describe '#valid?' do
+    context 'when initializerd with valid input without attachment and hastag' do
+      it 'should return true' do
+        post = Post.new({
+                          caption: "Saya akan semangat",
+                          user_id: 1
+                        })
+        expect(post.valid?).to eq(true)
       end
     end
   end
