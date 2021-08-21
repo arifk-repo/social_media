1b84a6355826b648ee86cc03e8d8cc5fff125488
root <root@DESKTOP-5EBSNVF.localdomain>
2021-08-21 08:16:13 +0700

    Add test get post by user id
    Signed-off-by: root <root@DESKTOP-5EBSNVF.localdomain>

--- post_spec.rb
+++ post_spec.rb
@@ -3,6 +3,14 @@ SimpleCov.start
 require './models/post'
 
 describe Post do
+  describe '#return data?' do
+    context 'when get post by user id' do
+      it 'should return all post created by user' do
+        post = Post.get_post_by_user_id(1)
+        expect(post[:post]).not_to be_nil
+      end
+    end
+  end
   describe '#valid?' do
     context 'when initializerd with valid input without attachment and hastag' do
       it 'should return true' do
