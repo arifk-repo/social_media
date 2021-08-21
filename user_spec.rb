16355546cb78be49121a798f5801c04cef3b329e
root <root@DESKTOP-5EBSNVF.localdomain>
2021-08-21 08:07:43 +0700

    Add test for add user
    Signed-off-by: root <root@DESKTOP-5EBSNVF.localdomain>

--- user_spec.rb
+++ user_spec.rb
@@ -1,13 +1,23 @@
 require 'simplecov'
 SimpleCov.start
 require './models/user'
-require './models/post'
-require './db/db_connector'
 
 describe User do
   describe '#valid?' do
-    context 'update user' do
-      it 'should not raise error' do
+
+    context 'when add new user' do
+      it 'should return true' do
+        user = User.new({
+                          username: "thomas12",
+                          email: "thomas12@gmail.com",
+                          bio: "thomas adalah teman arif"
+                        })
+        expect(user.save).to eq(true)
+      end
+    end
+
+    context 'user update' do
+      it 'should return true' do
         user = User.update_user({
                                   id: 2,
                                   username: "thomas13",
@@ -18,13 +28,27 @@ describe User do
       end
     end
 
+    context 'delete user by id' do
+      it 'should return true' do
+        user = User.delete(2)
+        expect(user).to eq(true)
+      end
+    end
+
     context 'get user by id' do
       it 'should return not nil' do
         user = User.get_user_by_id(1)
         expect(user).not_to be_nil
       end
-
     end
+
+    context 'get all user' do
+      it 'should return not nil' do
+        user = User.get_all_user
+        expect(user).not_to be_nil
+      end
+    end
+
     context 'when initializerd with valid input' do
       it 'should return update' do
         user = User.new({
@@ -68,50 +92,11 @@ describe User do
 
     context 'when initializerd with invalid email' do
       it 'should return false' do
-        client = connect_database
-        user2 =
-          user = User.new({
+        user = User.new({
                             username: "thomas12",
                             bio: "thomas adalah teman arif"
                           })
         expect(user.valid?).to eq(false)
-      end
-    end
-  end
-end
-
-describe Post do
-  describe '#valid?' do
-    context 'when initializerd with valid input without attachment and hastag' do
-      it 'should return true' do
-        post = Post.new({
-                          caption: "Saya akan semangat",
-                          user_id: 1
-                        })
-        expect(post.valid?).to eq(true)
-      end
-    end
-
-    context 'when initializerd with valid input without attachment' do
-      it 'should return true' do
-        post = Post.new({
-                          caption: "Saya akan semangat",
-                          user_id: 1,
-                          hastag: "#GenerasiGIGIH"
-                        })
-        expect(post.valid?).to eq(true)
-      end
-    end
-
-    context 'when initializerd with valid input' do
-      it 'should return true' do
-        post = Post.new({
-                          caption: "Saya akan semangat",
-                          user_id: 1,
-                          hastag: "#GenerasiGIGIH",
-                          attachment: "./media/data.csv"
-                        })
-        expect(post.valid?).to eq(true)
       end
     end
   end
